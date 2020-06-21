57518e4d801adb9138434b4961862e77fac898f6
anfetv.10 <anfetv.10@gmail.com>
2020-06-20 21:34:54 -0500

    avance movimiento de legumbre.

--- src/cosechaoperacionesv1/Juego.java
+++ src/cosechaoperacionesv1/Juego.java
@@ -72,13 +72,15 @@ public class Juego extends Application {
     
     public void actualizarEstado(){
         jugador.mover();
+        legumbre.mover();
     }
     
     public void inicializarComponentes() {
-        jugador = new Jugador(10, 400, 2, CANASTO);
-        legumbre = new Legumbre(10, 10, 1, LEGUMBRE);
+        jugador = new Jugador(10, 400, 7, CANASTO);
         root = new Group();
         escena = new Scene(root, 700, 500);
+        int numero = (int) (Math.random() * 600) + 1;
+        legumbre = new Legumbre(numero, -150, 1, LEGUMBRE);
         lienzo = new Canvas(700, 500);
         root.getChildren().add(lienzo);
         graficos = lienzo.getGraphicsContext2D();
@@ -87,7 +89,7 @@ public class Juego extends Application {
     
     public void pintar() {
         graficos.drawImage(new Image(FONDO), 0, 0);
-        jugador.pintar(graficos);        
+        jugador.pintar(graficos);
         legumbre.pintar(graficos);
     }
     
@@ -96,7 +98,7 @@ public class Juego extends Application {
             //se ejecuta cuando se presiona una tecla
             @Override
             public void handle(KeyEvent evento) {
-                System.out.println("tecla "+ evento.getCode());
+                //System.out.println("tecla "+ evento.getCode());
                 switch(evento.getCode().toString()){
                     case "RIGHT":
                        derecha = true;
@@ -115,7 +117,7 @@ public class Juego extends Application {
                        break;
                        
                     case "SPACE":
-                        jugador.setVelocidad(10);
+                        jugador.setVelocidad(20);
                         break;
                 }
             }
@@ -124,7 +126,7 @@ public class Juego extends Application {
             //se ejecuta cuando se suelta una tecla
             @Override
             public void handle(KeyEvent evento) {
-                System.out.println("tecla soltada "+ evento.getCode());
+                //System.out.println("tecla soltada "+ evento.getCode());
                 switch(evento.getCode().toString()){
                     case "RIGHT":
                        derecha = false;
@@ -141,9 +143,9 @@ public class Juego extends Application {
                     case "DOWN":
                        abajo = false;
                        break;  
-                       
+                    
                     case "SPACE":
-                        jugador.setVelocidad(4);
+                        jugador.setVelocidad(20);
                         break;
                 }
             }
